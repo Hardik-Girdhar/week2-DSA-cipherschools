f797ddc4374316c349fc65a9a239912ac7fe3ae1
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-06-22 21:46:15 +0530

    Add files via upload

---
+++ Lecture5-I-CipherSchool/lecture5-I.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+//without using arrays
+
+  int a,b,c,d,e;
+  cout<<"enter the values of 5 integers: "<<endl;
+  cin>>a>>b>>c>>d>>e;
+  cout<<"intergers in reverse order "<<e<<d<<c<<b<<a<<endl;
+  return 0;
+}
