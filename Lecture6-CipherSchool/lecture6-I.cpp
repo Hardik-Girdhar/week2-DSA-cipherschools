76168c497f949265313b76539a06d5df37a0e0b6
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-06-22 22:21:05 +0530

    Add files via upload

---
+++ Lecture6-CipherSchool/lecture6-I.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no. upto u want to print "<<endl;
+    cin>>n;
+    for(int i=0; i<=n ; i++){
+        cout<<i<<" ";
+    }
+  return 0;
+}
