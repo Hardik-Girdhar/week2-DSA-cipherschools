a0a4aef0ba2e7d618306f3ef7aa345ccda4bd5ac
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-06-22 21:27:14 +0530

    Add files via upload

---
+++ Lecture4-IICipherSchool/lecture4-I.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c,d;
+
+    cout<<"Input 4 integers  a,b,c and d:";
+    cout<<endl;
+    cin>>a>>b>>c>>d;
+   
+
+    // execute AND and OR statements and print out the Boolean: 
+    
+    //for AND condition:
+    cout<<(a>b && c>d);
+    
+    cout<<endl;
+    
+    //for OR condition:
+    cout<<(a>b || c>d);
+    
+    cout<<endl;
+
+    return 0;
+}
