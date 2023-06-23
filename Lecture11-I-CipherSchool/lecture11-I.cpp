33be8f641e1235c7bf7455c885970132f861f87b
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-06-23 20:04:37 +0530

    Add files via upload

---
+++ Lecture11-I-CipherSchool/lecture11-I.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"The size of int is: "<<sizeof(int)<<endl;   //bytes
+	cout<<"The size of long long int is: "<<sizeof(long long int); 
+	
+	return 0;
+}
