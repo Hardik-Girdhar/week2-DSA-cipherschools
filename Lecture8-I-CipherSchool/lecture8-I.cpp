298ced93e8d652c8630e9d68d767ef144acb480d
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-06-24 16:06:28 +0530

    Add files via upload

---
+++ Lecture8-I-CipherSchool/lecture8-I.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+//void printstuff(){
+//	cout<<"writing my first function";
+//}
+
+int sum(int a,int b){
+	return a+b;
+}
+
+int main(){
+//	printstuff();
+    int a,b;
+    a=3,b=5;
+    int ans=sum(a,b);
+    cout<<ans;
+	return 0;
+}
