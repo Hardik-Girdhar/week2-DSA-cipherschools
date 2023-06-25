f4a330033e1152b0dffa1a9974b3c5b9789f085d
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-06-25 16:53:10 +0530

    Add files via upload

---
+++ Lecture14-I-CipherSchool/lecture14-I-by_function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+    
+int main()
+{
+    int a[10];
+    
+    cout<<"value for comparison : "<<endl;
+    
+    cout<<"the value of a is "<<a<<endl;
+    cout<<"the value of address of a[0]"<<&a[0]<<endl;
+    cout<<"the value of address of a[1]"<<&a[1]<<endl;
+    
+    return 0;
+}
