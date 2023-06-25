3adc32f940a06c5b2560a483aa10dc0200789056
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-06-25 17:15:22 +0530

    Add files via upload

---
+++ Lecture15-II-CipherSchool/lecture15-II.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+    
+int main()
+{
+    string a;
+    string b;
+    string c;
+    
+    // int a;
+    // int b;
+    // int c;
+    
+    // cin>>a>>b;
+    
+    //cin>>a
+    
+    getline(cin,a);
+    
+    cout<<"the string which tou give as iput are "<<a<<b;
+    cout<<"your full name "<<a;    
+    // string c;
+    c=a+b;
+    
+    cout<<"the combined word is : "<<c<<endl;
+    
+    cout<<"length of cobined name is "<<c.length();
+    
+    return 0;
+}
