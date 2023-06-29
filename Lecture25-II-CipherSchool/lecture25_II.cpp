3b6439d8f0cb0be318f89fe8e1df5b1884e67dcd
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-06-29 20:13:32 +0530

    Add files via upload

---
+++ Lecture25-II-CipherSchool/lecture25_II.cpp
@@ -0,0 +1,33 @@
+#include<iostream
+using namespace std;
+
+class student
+{
+    public:
+
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+};
+
+class vehicle
+{
+    public:
+    string car_name;
+    string tyre_name;
+    int nummber_of_tyres;
+    int Number_of_seats;
+};
+
+int main()
+{
+
+    student a,b,c;
+
+    int d,e,f;
+
+    vehicle hydni , suzuki , ferrari;
+
+    return 0;
+}
