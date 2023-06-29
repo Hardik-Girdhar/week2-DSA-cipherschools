e9266911b6448286e1592ce0dfddeb20ff418bb8
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-06-29 20:11:41 +0530

    Add files via upload

---
+++ Lecture25-I-CipherSchool/lecture25-I.cpp
@@ -0,0 +1,18 @@
+#include<iostream
+using namespace std;
+
+int main()
+{
+
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+
+    string name_1;
+    int mids_1;
+    float ends_1;
+    bool pre_abs_1;
+
+    return 0;
+}
