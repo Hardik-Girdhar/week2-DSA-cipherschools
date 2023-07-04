d9d9fdce68581dbd4933e563e858f844b8c4768b
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-07-04 16:40:24 +0530

    Add files via upload

---
+++ Lecture31-II-CipherSchools/lecture31-II.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void mergesort(int *arr,int start , int )
+{
+    int mid;
+
+    mid = (start+end)/2;
+
+    mergesort()
+}
+
+int main()
+{
+    int size_arr;
+
+    inr arr[5] = {1,5,3,10,4};
+    size_arr=5;
+
+    mergesort(arr,0,size_arr-1);
+
+    return 0;
+}
