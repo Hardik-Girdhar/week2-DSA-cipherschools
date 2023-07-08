f0c918e57e460f8277729f3e616b6e1fe2740d5b
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-07-08 16:38:31 +0530

    Add files via upload

---
+++ Lecture34-I-CipherSchool/lecture34-I.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+
+    int data;
+    Node * left;
+    Node * right;
+    // Node * next3;
+};
+
+int main()
+{
+    string CEO;
+
+    string PA,CFO;
+
+    return 0;
+}
