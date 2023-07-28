94bd113bf473b707d8eea2ae9a3bfc0b5c0b6edb
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2023-07-28 14:50:16 +0530

    Create oop2.cpp

---
+++ oop2/oop2.cpp
@@ -0,0 +1,36 @@
+#include<iostream.h>
+using namespace std;
+
+Class SchoolBus
+{
+    private:
+
+    int number_tyres;
+    string name;
+    int capacity;
+    int milegage;
+
+    string schol_name;
+
+    int number_holidays;
+
+    public:
+
+    void calculatemileage()
+    {
+        cout<<"the mileage is"<<milegage;
+    }
+
+    void printnameofSchool()
+    {
+        cout<<"the name of the school is"<<name;
+    }
+};
+
+int main()
+{
+    // int marks;
+    // int attendance;
+    // string name;
+    return 0;
+}
